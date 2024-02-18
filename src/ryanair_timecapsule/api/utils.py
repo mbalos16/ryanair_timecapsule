d76ff597e94f8ffa30a62c7ffbd0ae3e2cdec7e6
Maria Balos <mariabalos16@gmail.com>
2024-02-18 22:17:22 +0000

    Add typing to call_api

--- src/ryanair_timecapsule/api/utils.py
+++ src/ryanair_timecapsule/api/utils.py
@@ -1,7 +1,10 @@
 import requests
+from requests.models import Response
 
 
-def call_api(url, params=None, return_json=True):
+def call_api(
+    url: str, params: dict = None, return_json: bool = True
+) -> dict | Response:
     response = requests.get(url=url, params=params)
     response.raise_for_status()
     if return_json:
