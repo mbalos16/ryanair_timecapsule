2892a370044d671f306f5a7ca1a67ff9fe9daf86
Maria Balos <mariabalos16@gmail.com>
2024-10-29 15:08:44 +0000

    Update call_api with headers parameters

--- src/ryanair_timecapsule/api/utils.py
+++ src/ryanair_timecapsule/api/utils.py
@@ -3,9 +3,9 @@ from requests.models import Response
 
 
 def call_api(
-    url: str, params: dict = None, return_json: bool = True
+    url: str, params: dict = None, return_json: bool = True, headers: dict = None
 ) -> dict | Response:
-    response = requests.get(url=url, params=params)
+    response = requests.get(url=url, params=params, headers=headers)
     response.raise_for_status()
     if return_json:
         return response.json()
--- test/ryanair_timecapsule/api/test_utils.py
+++ test/ryanair_timecapsule/api/test_utils.py
@@ -13,6 +13,13 @@ def mock_requests():
     adapter.register_uri("GET", "mock://test.com/json", text='{"a": "b"}')
     adapter.register_uri("GET", "mock://test.com/txt", text="abcd")
     adapter.register_uri("GET", "mock://test.com/error", status_code=555)
+    adapter.register_uri(
+        "GET",
+        "mock://test.com/headers",
+        request_headers={"key": "val"},
+        text="Headers received",
+    )
+
     return session.get
 
 
@@ -34,3 +41,23 @@ def test_call_api_error(monkeypatch):
     monkeypatch.setattr(requests, "get", mock_requests())
     with pytest.raises(HTTPError):
         call_api(url="mock://test.com/error")
+
+
+def test_call_api_headers(monkeypatch):
+    monkeypatch.setattr(requests, "get", mock_requests())
+    output = call_api(
+        url="mock://test.com/headers", headers={"key": "val"}, return_json=False
+    )
+    assert output.text == "Headers received"
+
+    # Ensure that an error is raised when the wrong headers are provided
+    with pytest.raises(requests_mock.exceptions.NoMockAddress):
+        call_api(
+            url="mock://test.com/headers",
+            headers={"wrong": "headers"},
+            return_json=False,
+        )
+
+    # Ensure that an error is raised when no headers are provided
+    with pytest.raises(requests_mock.exceptions.NoMockAddress):
+        call_api(url="mock://test.com/headers", return_json=False)
